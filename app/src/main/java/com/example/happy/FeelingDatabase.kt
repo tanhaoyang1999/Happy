83209f72b403295cf454fcbb40141e0a6a7c21e0
Tan Hao Yang <tanhaoyang1999@gmail.com>
2019-12-10 18:04:59 +0800

    initial commit

---
+++ app/src/main/java/com/example/happy/FeelingDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.happy
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Feeling::class), version = 1)
+abstract  class FeelingDatabase : RoomDatabase(){
+    //Instance of DAO
+    abstract fun feelingDao() : FeelingDao
+
+    //Ensure only one instance of the database is created
+    companion object{
+        @Volatile
+        private var INSTANCE: FeelingDatabase? = null
+
+        fun getDatabase(context: Context): FeelingDatabase{
+            val tempInstance = INSTANCE
+
+            if(tempInstance != null){
+                return tempInstance
+            }
+
+            //Create an instance of the database
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FeelingDatabase::class.java,
+                    "feeling_db"
+                ).build()
+                INSTANCE =instance
+                return instance
+            }
+        }
+    }
+}
